2c00a24e68223c74645978b1760a9bc377f1f23e
Nikunj <nikunjmi123@gmail.com>
2023-08-12 00:10:55 +0530

    version 0.8 of fancy calculator

--- fancy_Calc/main.js
+++ fancy_Calc/main.js
@@ -1,38 +1,41 @@
-const todoList = JSON.parse(localStorage.getItem("ourList")) ||[];
-displayList();
-document.querySelector('.todo-button').
-    addEventListener('click',() =>{
-        addTodo();
+let result = localStorage.getItem('calculation') || '';
+//  if local storage is undefined make result empty
+displayResult();
+document.querySelectorAll('.calc-button').forEach((calcButton, index) =>{
+    calcButton.addEventListener('click',() =>{
+        if(calcButton.innerHTML.length === 1 && calcButton.innerHTML != '=')
+          updateResult(calcButton.innerHTML);
     })
-function addTodo(){
-    let task=document.querySelector('.js-task');
-    let taskDate=document.querySelector('.js-date');
-    todoList.push({ name:task.value,
-                    dueDate:taskDate.value});
-    task.value='';
-    taskDate.value='';
-    displayList();
-    updatelocal();
-}
-function displayList(){
-    let showlist='';
-    for(let i =0;i<todoList.length; i++){
-        listElm = todoList[i];
-        showlist+=`
-        <div>${listElm.name} </div>
-        <div>${listElm.dueDate} </div>
-        <div> <button class="delete-button"
-        >Delete</button></div>`;
+})
+function updateResult(value){
+    if(result.length == 21){
+        alert('Sorry the calculator is fancy but it cant handle such large numbers')
     }
-    document.querySelector('.js-list').innerHTML=showlist;
-    document.querySelectorAll('.delete-button').forEach((deleteButton, index) =>{
-        deleteButton.addEventListener('click',() =>{
-            todoList.splice(index,1);
-            displayList();
-            updatelocal();
-        })
-    })
+    else{
+        result+=value;
+        localStorage.setItem('calculation',result);
+        displayResult();
+    }
 }
-function updatelocal(){
-    localStorage.setItem("ourList",JSON.stringify(todoList));
+function displayResult(){
+    document.querySelector('.result').innerHTML=result;
+}
+let resetButton = document.querySelector('.reset-button');
+let delButton = document.querySelector('.del-button');
+let resultButton = document.querySelector('.result-button');
+resultButton.addEventListener('click',()=>{
+    expressionEval();
+})
+delButton.addEventListener('click',()=>{
+    result=result.slice(0,-1);
+    displayResult();
+})
+resetButton.addEventListener('click',()=>{
+    result='';
+    displayResult();
+})
+function expressionEval(){
+    result=eval(result);
+    localStorage.setItem('calculation',result);
+    displayResult();
 }
